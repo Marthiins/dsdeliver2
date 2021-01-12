f20eba83c44b830d1bbfbdf901ece083d6efe639
Marthiins <sergio.marthiins@gmail.com>
2021-01-12 01:17:39 -0300

    Summary

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react';
 import { fetchProducts } from '../api';
 import Footer from '../Footer';
+import { checkIsSelected } from './helpers';
 import OrderLocation from './OrderLocation';
 import OrderSummary from './OrderSummary';
 import ProductsList from './ProductsList';
@@ -12,7 +13,12 @@ import { OrderLocationdata, Product } from './types';
 
 function Orders() {
 const[products, setProducts] = useState<Product[]>([]);
+const[selectedProducts, setSelectedProducts] = useState<Product[]>([]);
  const[orderLocation , setOrderLocation] = useState<OrderLocationdata>();
+const totalPrice = selectedProducts.reduce((sum, item) =>{
+  return sum + item.price;
+}, 0);
+
 
 useEffect(() => {
 fetchProducts()
@@ -20,13 +26,31 @@ fetchProducts()
 .catch(error => console.log(error))
   },[]);
 
+  const handleSelectProduct = (product: Product) => {
+    const isAlreadySelected = checkIsSelected(selectedProducts, product);
+  
+    if (isAlreadySelected) {
+      const selected = selectedProducts.filter(item => item.id !== product.id);
+      setSelectedProducts(selected);
+    } else {
+      setSelectedProducts(previous => [...previous, product]);
+    }
+  }
+
 return (
 <>
 <div className="orders-container">
   <StepsHeader />
-  <ProductsList products={products} />
+  <ProductsList
+   products={products}
+   onSelectProduct={handleSelectProduct}
+   selectedProducts = {selectedProducts}
+    />
   <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
-  <OrderSummary />
+  <OrderSummary 
+  amount={selectedProducts.length}
+  totalPrice={totalPrice} 
+  />
 </div>
 <Footer />
 </>
